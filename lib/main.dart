2f77e500fcef2c9efa3abe073f1bb08ef64264c6
tamaki8021 <hbk2002320@gmail.com>
2024-05-17 00:04:13 +0900

    modify: メールリンク認証のコードを削除in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:hibiki_tamaki_s_application1/firebase_options.dart';
-import 'package:hibiki_tamaki_s_application1/repositories/firebase_auth_repository_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:muse/firebase_options.dart';
+import 'package:muse/repositories/firebase_auth_repository_provider.dart';
+
 import 'core/app_export.dart';
 
-var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
+// var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -18,31 +19,6 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  // リンクからアプリへ遷移するとき、アプリが開いていないと発動
-  await FirebaseDynamicLinks.instance.getInitialLink().then((data) async {
-    final String? deepLink = data?.link.toString();
-    // メールアドレスの入力がない場合はスキップ
-    final email = 'hbk14320@gmail.com';
-    final auth = FirebaseAuth.instance;
-
-    if (deepLink == null) return;
-    if (auth.isSignInWithEmailLink(deepLink)) {
-      // メールリンクに含まれる認証情報でサインイン
-      // 成功したらFirebase Authenticationにユーザーを作成（すでに存在する場合はログインのみ）
-      await auth
-          .signInWithEmailLink(email: email, emailLink: deepLink)
-          .then((credential) {
-        // TODO: Firestoreへuserを登録する(既に存在している場合は行わない)
-        // You can access the new user via userCredential.user.
-        final emailAddress = credential.user?.email;
-        print(emailAddress);
-      });
-    }
-    print('nitiallink');
-  }).catchError((e) {
-    print(e);
-  });
 
   Future.wait([
     SystemChrome.setPreferredOrientations([
