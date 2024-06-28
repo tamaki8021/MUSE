5a6a3ff8466f4f879043850ce0239ea76e37d7d4
tamaki8021 <hbk2002320@gmail.com>
2024-06-28 14:02:14 +0900

    feat: bnbの組み込み実装

---
+++ lib/presentation/main_app_navigator.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:muse/core/app_export.dart';
+import 'package:muse/presentation/chat_page/chat_page.dart';
+import 'package:muse/presentation/home_page/home_page.dart';
+import 'package:muse/presentation/profile_page/profile_page.dart';
+import 'package:muse/widgets/bottom_bar/custom_bottom_bar.dart';
+
+class MainAppNavigator extends StatelessWidget {
+  const MainAppNavigator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final currentRoute = ModalRoute.of(context)?.settings.name;
+
+    return SafeArea(
+      child: Scaffold(
+        extendBody: true,
+        extendBodyBehindAppBar: true,
+        body: getCurrentPage(context, currentRoute!),
+        bottomNavigationBar: const CustomBottomBar(),
+      ),
+    );
+  }
+
+  Widget getCurrentPage(BuildContext context, String currentRoute) {
+    switch (currentRoute) {
+      case AppRoutes.homePage:
+        return const HomePage();
+      case AppRoutes.chatPage:
+        return const ChatPage();
+      case AppRoutes.profilePage:
+        return const ProfilePage();
+      default:
+        return const SizedBox.shrink();
+    }
+  }
+}
