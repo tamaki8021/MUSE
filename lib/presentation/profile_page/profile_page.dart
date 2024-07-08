7e7bbf233aaa68b938fec42a683e4e782663951d
tamaki8021 <hbk2002320@gmail.com>
2024-07-08 16:50:47 +0900

    feat: profileページのUI作成

--- lib/presentation/profile_page/profile_page.dart
+++ lib/presentation/profile_page/profile_page.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:muse/core/app_export.dart';
-import '../profile_page/widgets/thirty_item_widget.dart';
-import '../profile_page/widgets/userprofile1_item_widget.dart';
-import 'models/thirty_item_model.dart';
-import 'models/userprofile1_item_model.dart';
-import 'notifier/profile_notifier.dart';
+import 'package:muse/widgets/custom_tab_bar.dart';
 
-// ignore_for_file: must_be_immutable
-class ProfilePage extends ConsumerStatefulWidget {
-  const ProfilePage({Key? key})
-      : super(
-          key: key,
-        );
+import 'widgets/music_cover_grid_view.dart';
+import 'widgets/user_image_row.dart';
+import 'widgets/user_info.dart';
+
+class ProfilePage extends HookConsumerWidget {
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  ProfilePageState createState() => ProfilePageState();
-}
-
-class ProfilePageState extends ConsumerState<ProfilePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return SafeArea(
       child: Scaffold(
         extendBody: true,
@@ -29,161 +21,24 @@ class ProfilePageState extends ConsumerState<ProfilePage> {
         body: Container(
           width: SizeUtils.width,
           height: SizeUtils.height,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: const Alignment(-0.06, -0.2),
-              end: const Alignment(1.37, 0.82),
-              colors: [
-                appTheme.purpleA200,
-                appTheme.purple800,
-                appTheme.black900,
-              ],
-            ),
-          ),
-          child: Container(
-            width: double.maxFinite,
-            padding: EdgeInsets.symmetric(vertical: 3.h),
-            child: Column(
-              children: [
-                SizedBox(height: 33.h),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 51.h),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: 26.h,
-                          bottom: 36.h,
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              'lbl_1_342'.tr,
-                              style: TextStyle(
-                                color: appTheme.white,
-                                fontSize: 16.fSize,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            SizedBox(height: 3.h),
-                            Text(
-                              'lbl_my_posts'.tr,
-                              style: TextStyle(
-                                color: appTheme.blueGray300,
-                                fontSize: 12.fSize,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Spacer(
-                        flex: 52,
-                      ),
-                      SizedBox(
-                        height: 108.h,
-                        width: 96.h,
-                        child: Stack(
-                          alignment: Alignment.topCenter,
-                          children: [
-                            CustomImageView(
-                              imagePath: ImageConstant.imgEllipse3096x96,
-                              height: 96.adaptSize,
-                              width: 96.adaptSize,
-                              radius: BorderRadius.circular(
-                                48.h,
-                              ),
-                              alignment: Alignment.topCenter,
-                            ),
-                            CustomImageView(
-                              imagePath: ImageConstant.imgImage88x88,
-                              height: 88.adaptSize,
-                              width: 88.adaptSize,
-                              radius: BorderRadius.circular(
-                                44.h,
-                              ),
-                              alignment: Alignment.topCenter,
-                              margin: EdgeInsets.only(top: 4.h),
-                            ),
-                            CustomIconButton(
-                              height: 24.adaptSize,
-                              width: 24.adaptSize,
-                              padding: EdgeInsets.all(4.h),
-                              decoration: IconButtonStyleHelper.outlineWhiteA,
-                              alignment: Alignment.bottomCenter,
-                              child: CustomImageView(
-                                imagePath: ImageConstant.imgCheckmark,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Spacer(
-                        flex: 47,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: 25.h,
-                          bottom: 36.h,
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              'lbl_586'.tr,
-                              style: TextStyle(
-                                color: appTheme.white,
-                                fontSize: 16.fSize,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            SizedBox(height: 2.h),
-                            Text(
-                              'lbl_favorites'.tr,
-                              style: TextStyle(
-                                color: appTheme.blueGray300,
-                                fontSize: 12.fSize,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 9.h),
-                Text(
-                  'lbl_eleanor_mena'.tr,
-                  style: TextStyle(
-                    color: appTheme.white,
-                    fontSize: 18.fSize,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(height: 8.h),
-                Text(
-                  'msg_sometimes_i_cook'.tr,
-                  style: TextStyle(
-                    color: appTheme.blueGray300,
-                    fontSize: 14.fSize,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                SizedBox(height: 30.h),
-                _buildUserProfile(context),
-                SizedBox(height: 17.h),
-                _buildTabsTabBar(context),
-                SizedBox(height: 17.h),
-                _buildThirty(context),
-              ],
+          decoration: AppDecoration.gradientBackground,
+          padding: EdgeInsets.only(top: 6.h),
+          child: DefaultTabController(
+            length: 2,
+            child: NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return [
+                  _buildHeaderSection(),
+                  _buildTabBar(),
+                ];
+              },
+              body: const TabBarView(
+                children: [
+                  MusicCoverGridView(),
+                  MusicCoverGridView(),
+                ],
+              ),
             ),
           ),
         ),
@@ -191,154 +46,65 @@ class ProfilePageState extends ConsumerState<ProfilePage> {
     );
   }
 
-  /// Section Widget
-  PreferredSizeWidget _buildAppBar(BuildContext context) {
-    return CustomAppBar(
-      centerTitle: true,
-      title: AppbarTitle(
-        text: 'lbl_muse'.tr,
-      ),
-      actions: [
-        AppbarTrailingImage(
-          imagePath: ImageConstant.imgUser,
-          margin: EdgeInsets.symmetric(
-            horizontal: 24.h,
-            vertical: 4.h,
-          ),
-        ),
-      ],
-    );
-  }
-
-  /// Section Widget
-  Widget _buildUserProfile(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: 29.h,
-        right: 33.h,
-      ),
-      child: Consumer(
-        builder: (context, ref, _) {
-          return ListView.separated(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            separatorBuilder: (
-              context,
-              index,
-            ) {
-              return SizedBox(
-                height: 17.h,
-              );
-            },
-            itemCount: ref
-                    .watch(profileNotifier)
-                    .profileModelObj
-                    ?.userprofile1ItemList
-                    .length ??
-                0,
-            itemBuilder: (context, index) {
-              final model = ref
-                      .watch(profileNotifier)
-                      .profileModelObj
-                      ?.userprofile1ItemList[index] ??
-                  Userprofile1ItemModel();
-              return Userprofile1ItemWidget(
-                model,
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  /// Section Widget
-  Widget _buildTabsTabBar(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      padding: EdgeInsets.fromLTRB(74.h, 7.h, 74.h, 6.h),
-      decoration: AppDecoration.outlineGray,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 3.h),
-            child: Text(
-              'lbl_my_post'.tr,
-              style: TextStyle(
-                color: appTheme.yellow900,
-                fontSize: 14.fSize,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          const Spacer(
-            flex: 43,
-          ),
-          SizedBox(
-            height: 24.h,
-            child: VerticalDivider(
-              width: 1.h,
-              thickness: 1.h,
-            ),
-          ),
-          const Spacer(
-            flex: 56,
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 2.h),
-            child: Text(
-              'lbl_favorite'.tr,
-              style: TextStyle(
-                color: appTheme.white,
-                fontSize: 14.fSize,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
+  Widget _buildHeaderSection() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        [
+          const UserInfo(),
+          UserImageRow(label: 'lbl_friends'.tr),
         ],
       ),
     );
   }
 
-  /// Section Widget
-  Widget _buildThirty(BuildContext context) {
-    return SizedBox(
-      height: 170.h,
-      child: Consumer(
-        builder: (context, ref, _) {
-          return ListView.separated(
-            padding: EdgeInsets.symmetric(horizontal: 15.h),
-            scrollDirection: Axis.horizontal,
-            separatorBuilder: (
-              context,
-              index,
-            ) {
-              return SizedBox(
-                width: 23.h,
-              );
-            },
-            itemCount: ref
-                    .watch(profileNotifier)
-                    .profileModelObj
-                    ?.thirtyItemList
-                    .length ??
-                0,
-            itemBuilder: (context, index) {
-              final model = ref
-                      .watch(profileNotifier)
-                      .profileModelObj
-                      ?.thirtyItemList[index] ??
-                  ThirtyItemModel();
-              return ThirtyItemWidget(
-                model,
-              );
-            },
-          );
-        },
+  Widget _buildTabBar() {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _StickyTabBarDelegate(
+        tabBar: CustomTabBar(
+          leftLabel: 'lbl_post'.tr,
+          rightLabel: 'lbl_favorite'.tr,
+        ),
       ),
     );
+  }
+
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return CustomAppBar(
+      centerTitle: true,
+      title: AppbarTitle(
+        text: 'lbl_profile'.tr,
+      ),
+      actions: [
+        AppbarTrailingImage(imagePath: ImageConstant.imgUser),
+      ],
+    );
+  }
+}
+
+/// タブバーをヘッダーに配置するためのカスタムデリゲート
+class _StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  const _StickyTabBarDelegate({required this.tabBar});
+
+  final PreferredSizeWidget tabBar;
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return tabBar;
+  }
+
+  @override
+  bool shouldRebuild(_StickyTabBarDelegate oldDelegate) {
+    return tabBar != oldDelegate.tabBar;
   }
 }
---
+++ lib/presentation/profile_page/widgets/music_cover_grid_view.dart
@@ -0,0 +1,33 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class MusicCoverGridView extends StatelessWidget {
+  const MusicCoverGridView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> imageUrls = List.generate(
+      30,
+      (index) => 'https://picsum.photos/seed/$index/200/200',
+    );
+
+    return GridView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+      itemCount: imageUrls.length,
+      itemBuilder: (context, index) {
+        return CachedNetworkImage(
+          imageUrl: imageUrls[index],
+          placeholder: (context, url) => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          fit: BoxFit.cover,
+        );
+      },
+    );
+  }
+}
