870b634671442d95f209e14b3a1a4f5ca1934ea8
tamaki8021 <hbk2002320@gmail.com>
2024-07-14 12:28:31 +0900

    feat: activity_pageの雛形を作成

---
+++ lib/presentation/activity_page/activity_pade.dart
@@ -0,0 +1,11 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+class ActivityPage extends StatelessWidget {
+  const ActivityPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
