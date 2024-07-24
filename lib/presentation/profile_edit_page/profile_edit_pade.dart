c4f263e935038a21a081973181fc45c45711b984
tamaki8021 <hbk2002320@gmail.com>
2024-07-24 23:25:16 +0900

    refactor: extract method

--- lib/presentation/profile_edit_page/profile_edit_pade.dart
+++ lib/presentation/profile_edit_page/profile_edit_pade.dart
@@ -20,76 +20,80 @@ class ProfileEditPage extends StatelessWidget {
         extendBody: true,
         extendBodyBehindAppBar: true,
         backgroundColor: Colors.transparent,
-        body: Container(
-          width: SizeUtils.width,
-          height: SizeUtils.height,
-          color: appTheme.black900,
-          padding: EdgeInsets.only(top: 6.h),
-          child: Column(
-            children: [
-              const Spacer(),
-              CustomInputImage(
-                radius: 18.adaptSize,
-              ),
-              SizedBox(height: 3.h),
-              CustomTextFormField(
-                contentPadding: const EdgeInsets.all(15),
-                textStyle: CustomTextStyles.bodySmallWhite,
-                labelText: 'lbl_name'.tr,
-                hintText: 'hint_name'.tr,
-                labelStyle: CustomTextStyles.labelSmallInter,
-                hintStyle: CustomTextStyles.labelSmallInterGray500,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'validator_mes_name_is_empty'.tr;
-                  }
-                  return null;
-                },
-                borderDecoration: InputBorderStyle.outlinedBorderWhiteCircle10,
-                fillColor: Colors.transparent,
-              ),
-              SizedBox(height: 2.h),
-              CustomTextFormField(
-                labelText: 'lbl_bio'.tr,
-                labelStyle: CustomTextStyles.labelSmallInter,
-                textStyle: CustomTextStyles.bodySmallWhite,
-                hintText: 'hint_bio'.tr,
-                hintStyle: CustomTextStyles.labelSmallInterGray500,
-                contentPadding: const EdgeInsets.all(15),
-                maxLines: 3,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'validator_mes_bio_is_empty'.tr;
-                  } else if (value.length > 200) {
-                    return 'validator_mes_bio_lenght'.tr;
-                  }
-                  return null;
-                },
-                borderDecoration: InputBorderStyle.outlinedBorderWhiteCircle10,
-                fillColor: Colors.transparent,
-              ),
-              const Spacer(flex: 6),
-              SizedBox(
-                width: SizeUtils.width,
-                height: 7.h,
-                child: OutlineGradientButton(
-                  gradient: AppDecoration.appGradient.gradient!,
-                  strokeWidth: 1,
-                  radius: const Radius.circular(10),
-                  child: Center(
-                    child: CustomText(
-                      text: 'lbl_done'.tr,
-                      isGradient: true,
-                      style: CustomTextStyles.bodySmallWhite
-                          .copyWith(fontWeight: FontWeight.w600),
-                    ),
-                  ),
+        body: _buildBody(),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      width: SizeUtils.width,
+      height: SizeUtils.height,
+      color: appTheme.black900,
+      padding: EdgeInsets.only(top: 6.h),
+      child: Column(
+        children: [
+          const Spacer(),
+          CustomInputImage(
+            radius: 18.adaptSize,
+          ),
+          SizedBox(height: 3.h),
+          CustomTextFormField(
+            contentPadding: const EdgeInsets.all(15),
+            textStyle: CustomTextStyles.bodySmallWhite,
+            labelText: 'lbl_name'.tr,
+            hintText: 'hint_name'.tr,
+            labelStyle: CustomTextStyles.labelSmallInter,
+            hintStyle: CustomTextStyles.labelSmallInterGray500,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'validator_mes_name_is_empty'.tr;
+              }
+              return null;
+            },
+            borderDecoration: InputBorderStyle.outlinedBorderWhiteCircle10,
+            fillColor: Colors.transparent,
+          ),
+          SizedBox(height: 2.h),
+          CustomTextFormField(
+            labelText: 'lbl_bio'.tr,
+            labelStyle: CustomTextStyles.labelSmallInter,
+            textStyle: CustomTextStyles.bodySmallWhite,
+            hintText: 'hint_bio'.tr,
+            hintStyle: CustomTextStyles.labelSmallInterGray500,
+            contentPadding: const EdgeInsets.all(15),
+            maxLines: 3,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'validator_mes_bio_is_empty'.tr;
+              } else if (value.length > 200) {
+                return 'validator_mes_bio_lenght'.tr;
+              }
+              return null;
+            },
+            borderDecoration: InputBorderStyle.outlinedBorderWhiteCircle10,
+            fillColor: Colors.transparent,
+          ),
+          const Spacer(flex: 6),
+          SizedBox(
+            width: SizeUtils.width,
+            height: 7.h,
+            child: OutlineGradientButton(
+              gradient: AppDecoration.appGradient.gradient!,
+              strokeWidth: 1,
+              radius: const Radius.circular(10),
+              child: Center(
+                child: CustomText(
+                  text: 'lbl_done'.tr,
+                  isGradient: true,
+                  style: CustomTextStyles.bodySmallWhite
+                      .copyWith(fontWeight: FontWeight.w600),
                 ),
               ),
-              const Spacer(),
-            ],
+            ),
           ),
-        ),
+          const Spacer(),
+        ],
       ),
     );
   }
