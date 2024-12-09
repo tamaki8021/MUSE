402b550f65c8bd1e01cad805a0819ec4ee4f85a9
tamaki8021 <hbk2002320@gmail.com>
2024-12-10 01:05:09 +0900

    feat: open snackbar widget

---
+++ lib/widgets/common/snack_bar.dart
@@ -0,0 +1,22 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:muse/core/app_export.dart';
+
+void openSnackBar({
+  required BuildContext context,
+  required String message,
+  Color? backgroundColor,
+  Color? textColor,
+}) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        message,
+        style: CustomTextStyles.bodySmallWhite,
+      ),
+      backgroundColor: backgroundColor ?? Colors.red,
+    ),
+  );
+}
